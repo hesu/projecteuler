b0bed3aa78dd812073cfc24574dc709c9fa70e1e
이혜수 <hesu@ihyesuui-MacBook-Air.local>
2016-01-11 10:06:03 +0900

    modified:   main.cpp

--- euler37/main.cpp
+++ euler37/main.cpp
@@ -5,32 +5,51 @@
 #include <iostream>
 #include <ctime>
 #include <map>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
-std::map<int,int> primes;
-bool isPrimeBothSides( int n)
+std::map<int,int> bothSidePrimes;
+bool isBothSidePrime( int n)
 {
 	if( n == 2) return true;
-
-  std::map<int,int>::iterator it = primes.find(n);
-  if( it != primes.end()) { return true; }
-
+  std::map<int,int>::iterator it = bothSidePrimes.find(n);
+  if( it != bothSidePrimes.end()) { return true; }
   int s = sqrt( n) + 1;
   for(int i=s; i>1; i--)
   {
     if (n%i ==0) { return false; }
   }
 
-	primes.insert( std::map<int,int>::value_type( n, n));
+	if( n < 10) { 
+		bothSidePrimes.insert( std::map<int,int>::value_type( n, n));
+		return true;
+	}
+
+	char buf[1024] = {0,};
+	sprintf( buf, "%d", n);
+	int r = 0;
+	for(int i=0; i<strlen(buf); i++)
+	{
+		r += pow( 10, i) * ( buf[i] - '0'); 
+	}
+
+	s  = sqrt( r) + 1;
+	for(int i=s; i>1; i--)
+	{
+  	if (r%i ==0) { return false; }
+	}
+
+	bothSidePrimes.insert( std::map<int,int>::value_type( n, n));
+	bothSidePrimes.insert( std::map<int,int>::value_type( r, r));
   return true;
 }
 
-
+std::vector<int> sum;
 int main(int argc, char** argv)
 {
   clock_t begin = clock();
-
   /* starting code */
 
   /* end of code */
