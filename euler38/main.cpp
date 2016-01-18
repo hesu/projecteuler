a5e26085feccd3535e5109299661719d1162dcc2
hesu <hesuslee@gmail.com>
2016-01-19 00:09:16 +0900

    modified:   main.cpp

--- euler38/main.cpp
+++ euler38/main.cpp
@@ -7,10 +7,30 @@
 
 using namespace std;
 
+
+bool isPandigital( char arr[])
+{
+	bool swit[10] = {false,};
+	for(int i=0; i<10; i++)
+	{
+		int idx = arr[i] - '0';
+		if (swit[idx] == true) { return false; }
+		else { swit[idx] = true; }
+	}
+
+	for(int i=0; i<10; i++)
+	{
+		if( swit[i] == false) {return false; }
+	}
+	return true;
+}
+
 int main(int argc, char** argv)
 {
   clock_t begin = clock();
-  /* starting code */
+ 
+	/* starting code */
+
   /* end of code */
   clock_t end = clock();
   std::cout << "elapsed time=" << double(end - begin) / CLOCKS_PER_SEC << std::endl;
