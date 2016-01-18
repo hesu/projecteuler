46d25e7e03060dc97203e635dd7fcabf28658701
hesu <hesuslee@gmail.com>
2016-01-18 23:20:53 +0900

    new file:   Makefile 	new file:   main.cpp

---
+++ euler38/main.cpp
@@ -0,0 +1,18 @@
+/*
+  Problem 38 - Pandigital multiples
+*/
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  clock_t begin = clock();
+  /* starting code */
+  /* end of code */
+  clock_t end = clock();
+  std::cout << "elapsed time=" << double(end - begin) / CLOCKS_PER_SEC << std::endl;
+  return 0;
+}
