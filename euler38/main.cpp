fcf848fa5ff2831786fcfb5e2e1e09727115d2a1
이혜수 <hesu@ihyesuui-MacBook-Air.local>
2016-01-19 13:24:11 +0900

    deleted:    euler37/Makefile 	modified:   euler38/main.cpp

--- euler38/main.cpp
+++ euler38/main.cpp
@@ -10,6 +10,8 @@ using namespace std;
 
 bool isPandigital( char arr[])
 {
+	if (strlen(arr) != 10) { return false; }
+
 	bool swit[10] = {false,};
 	for(int i=0; i<10; i++)
 	{
@@ -25,11 +27,34 @@ bool isPandigital( char arr[])
 	return true;
 }
 
+void multiplesAttach( char buf[], int n)
+{
+	buf[0] = '0';
+	char* c = &buf[1];
+	for(int i=1;; i++)
+	{
+		int mul = n*i;
+		c += sprintf( c, "%d", mul);
+		int len = strlen( buf);
+		if (len >= 10) { return; }
+	}
+}
+
 int main(int argc, char** argv)
 {
   clock_t begin = clock();
  
 	/* starting code */
+
+	// 10000 * 1 = 10000, 10000 * 2 = 20000 (1000020000 : 10개. Pandigital 자리수 초과!)
+	for(int i=1; i<9999; i++)
+	{
+		char s[32] = {0,};
+		multiplesAttach( s, i);
+		if (isPandigital( s)) {
+			cout << "pandigital at i=" << i << " s=" << s << endl;
+		}
+	}
 
   /* end of code */
   clock_t end = clock();
