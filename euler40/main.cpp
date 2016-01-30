017455c3d3dd8784651e2d6b40515cafd93d49f6
hesu <hesuslee@gmail.com>
2016-01-30 09:32:33 +0900

    modified:   main.cpp

--- euler40/main.cpp
+++ euler40/main.cpp
@@ -2,17 +2,41 @@
   Problem 40 - Chmpernowne's constant 
 */
 
-// TODO
-
 #include <iostream>
 #include <ctime>
 #include <math.h>
+#include <stdio.h>
 
 using namespace std;
 
-int getStartingDigit( int n, int scopes[]) 
+#define MAXSIZE 10
+
+int getLeftAndStartingDigit( int n, int scopes[], int* digit) 
 {
-	return scopes[n-1]; 
+	int i;
+	for(i=0; i<MAXSIZE; i++)
+	{
+		if (n < scopes[i]) {
+			*digit = i;
+			break;
+		}
+	}
+	return (n - scopes[i-1]); 
+}
+
+int getDn( int left, int startingDigit)
+{
+	int n = pow(10, startingDigit-1); // starting number
+
+	for(;left>startingDigit; left-=startingDigit)
+	{
+		n++;	
+	}
+
+	char buf[32] = {0,};
+	sprintf( buf, "%d", n);
+	int dn = buf[left-1] - '0';
+	return dn;
 }
 
 int main(int argc, char** argv)
@@ -25,7 +49,7 @@ int main(int argc, char** argv)
 	scopes[0] = 1;
 
 	int sum = 0;
-	for(int i = 1; i< 10; i++)
+	for(int i = 1; i< MAXSIZE; i++)
 	{
 		sum += (pow( 10, i) - pow( 10, i-1)) * i;
 		scopes[i] = sum; 
@@ -34,9 +58,14 @@ int main(int argc, char** argv)
 	int products = 1;
 	for(int i=1; i<=6; i++)
 	{
-		int digit = getStartingDigit( pow(10, i), scopes);
-		cout << "at pow(10," << i << ")" << ", digit=" << digit << endl;
+		int n = pow(10, i);
+		int d;
+		int left = getLeftAndStartingDigit( n, scopes, &d);
+
+		cout << "n=" << n << " left=" << left << " digit=" << d << endl;
+		products *= getDn( left, d);
 	}
+	cout << endl << "products of dn = " << products << endl;
 
   /* end of code */
 
