ffd69189a70a595c7d15ba26297ab5aa2cce479f
hesu <hesuslee@gmail.com>
2016-02-03 00:50:08 +0900

    modified:   euler16/main.cpp 	modified:   euler20/main.cpp

--- euler16/main.cpp
+++ euler16/main.cpp
@@ -1,6 +1,6 @@
 /*
-    Problem 16 - Power digit sum
-*/
+	 Problem 16 - Power digit sum
+ */
 
 #include <iostream>
 #include <cmath>
@@ -12,35 +12,35 @@ using namespace std;
 
 void multiply( int result[], int mul, int maxlen)
 {
-    int carry = 0;
-    for(int i=0; i<maxlen; i++)
-    {
-        //cout << "i=" << i << " carry=" << carry << endl;
-        int prev_carry = carry;
-        int val = result[i] * mul;
-        carry = val/10;
-        result[i] = val%10 + prev_carry;
-    }
+	int carry = 0;
+	for(int i=0; i<maxlen; i++)
+	{
+		//cout << "i=" << i << " carry=" << carry << endl;
+		int prev_carry = carry;
+		int val = result[i] * mul;
+		carry = val/10;
+		result[i] = val%10 + prev_carry;
+	}
 }
 
 int main(int argc, char** argv)
 {
-    int sum[1000] = {0,};
-    sum[0] = 1;
+	int sum[1000] = {0,};
+	sum[0] = 1;
 
-    for(int i=0; i<1000; i++)
-    {
-        multiply( sum, 2, 1000);
-        //for(int i=0; i<10; i++) { cout << sum[i] << " "; }
-        cout << endl;
-    }
+	for(int i=0; i<1000; i++)
+	{
+		multiply( sum, 2, 1000);
+		//for(int i=0; i<10; i++) { cout << sum[i] << " "; }
+		cout << endl;
+	}
 
-    int nsum = 0;
-    for(int i=0; i<1000; i++)
-    {
-        nsum += sum[i];
-    }
+	int nsum = 0;
+	for(int i=0; i<1000; i++)
+	{
+		nsum += sum[i];
+	}
 
-    cout << "nsum=" << nsum << endl;
-    return 0;
+	cout << "nsum=" << nsum << endl;
+	return 0;
 }
--- euler20/main.cpp
+++ euler20/main.cpp
@@ -1,6 +1,6 @@
 /*
-    Problem 20 - Factorial digit sum
-*/
+	 Problem 20 - Factorial digit sum
+ */
 
 #include <iostream>
 
@@ -8,46 +8,46 @@ using namespace std;
 
 void multiply( int result[], int mul, int maxlen)
 {
-    for(int i=0; i<maxlen; i++)
-    {
-        //cout << "i=" << i << " carry=" << carry << endl;
-        //int val = result[i] * mul;
-        result[i] = result[i] * mul;
-    }
+	for(int i=0; i<maxlen; i++)
+	{
+		//cout << "i=" << i << " carry=" << carry << endl;
+		//int val = result[i] * mul;
+		result[i] = result[i] * mul;
+	}
 
-    // trim
-    int nextcarry = 0;
-    for(int i=0; i<maxlen; i++)
-    {
-        int carry = nextcarry;
-        nextcarry = result[i]/10;
-        result[i] = result[i]%10 + carry;
+	// trim
+	int nextcarry = 0;
+	for(int i=0; i<maxlen; i++)
+	{
+		int carry = nextcarry;
+		nextcarry = result[i]/10;
+		result[i] = result[i]%10 + carry;
 
-        while( result[i]>=10) {
-            result[i] = result[i]-10;
-            nextcarry++;
-        }
-    }
+		while( result[i]>=10) {
+			result[i] = result[i]-10;
+			nextcarry++;
+		}
+	}
 }
 
 int main(int argc, char** argv)
 {
-    int fac[1000] = {0,};
-    fac[0] = 1;
+	int fac[1000] = {0,};
+	fac[0] = 1;
 
-    for(int i=1; i<=100; i++)
-    {
-        multiply( fac, i, 1000);
-    }
+	for(int i=1; i<=100; i++)
+	{
+		multiply( fac, i, 1000);
+	}
 
-    int nsum = 0;
-    for(int i=0; i<1000; i++)
-    {
-        cout << fac[i] << " ";
-        nsum += fac[i];
-    }
-    cout << endl;
+	int nsum = 0;
+	for(int i=0; i<1000; i++)
+	{
+		cout << fac[i] << " ";
+		nsum += fac[i];
+	}
+	cout << endl;
 
-    cout << "nsum=" << nsum << endl;
-    return 0;
+	cout << "nsum=" << nsum << endl;
+	return 0;
 }
