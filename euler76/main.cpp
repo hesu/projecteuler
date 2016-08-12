ec80eb92fc65266364802884ee81bed52773b880
hesu <hesuslee@gmail.com>
2016-08-12 21:03:17 +0900

    modified:   main.cpp

--- euler76/main.cpp
+++ euler76/main.cpp
@@ -4,12 +4,31 @@
 	
 #include <iostream>
 #include <ctime>
+#include <map>
+#include <vector>
+
 using namespace std;
+
+int countingSummations( int n)
+{
+  cout << "n=" << n << endl;
+  if( n == 0) return 0;
+  if( n <= 2) return 1;
+
+  if( n % 2 == 0) {
+    return countingSummations(n-1) + 1;
+  } else {
+    return countingSummations(n-1) + 2;
+  }
+}
 
 int main(int argc, char** argv)
 {
 	clock_t begin = clock();
 	/* starting code */
+
+  int n = countingSummations(100);
+  cout << "summations=" << n << endl;
 	/* end of code */
 	clock_t end = clock();
 	std::cout << "elapsed time=" << double(end - begin) / CLOCKS_PER_SEC << std::endl;
