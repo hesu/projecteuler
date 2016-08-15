31e868bb13eba6979022d4bc9e16c79720d60f32
hesu <hesuslee@gmail.com>
2016-08-15 10:25:27 +0900

    not solved yet 	modified:   main.cpp

--- euler76/main.cpp
+++ euler76/main.cpp
@@ -9,16 +9,43 @@
 
 using namespace std;
 
-int coinsum( std::vector<int> coins, int val)
+
+unsigned int memo[100][100] = {0};
+
+unsigned int coinsum( std::vector<int> coins, int val)
 {
   // c(n, s(m)) = c(n, s(m-1)) + c(n-m, s(m));
   if( coins.size() == 0) return 0;
   if( val < 0 ) return 0;
   if( val == 0) return 1;
+
+  unsigned int a;
+
+  int idxa = coins[ coins.size() - 2];
+  if( memo[ idxa][ val] != 0) {
+    a = memo[ idxa][ val];
+  } else {
+    std::vector<int> c(coins);
+    c.pop_back();
+    a = coinsum( c, val);
+  }
+
+  unsigned int b;
+
+  int idxb = coins[ coins.size()-1];
+  if( memo[ idxb][ val - idxb] != 0) {
+    b = memo[ idxb][ val - idxb]; 
+  } else {
+    b = coinsum( coins, val - idxb);
+  }
   
-  std::vector<int> c(coins);
-  c.pop_back();
-  return coinsum( c, val) + coinsum( coins, val - coins[ coins.size()-1]);
+  unsigned int ret = a + b;
+  if( memo[ idxb][ val] == 0) { 
+    cout << "coins.size()-1=" << idxb << " val=" << val << " for ret=" << ret << endl;
+    memo[ idxb][ val] = ret; 
+  }
+
+  return ret;
 }
 
 int main(int argc, char** argv)
@@ -28,13 +55,10 @@ int main(int argc, char** argv)
 	/* starting code */
 	std::vector<int> coins;
 
-  for(int i=1; i<=100; i++)
-  {
-    coins.push_back(i);
-  }
+  for(int i=1; i<=99; i++) coins.push_back(i);
 
   int howmuch = 100;
-  int sum = coinsum( coins, howmuch);
+  unsigned int sum = coinsum( coins, howmuch);
 
   cout << "val=" << howmuch << endl;
   cout << "coin sum=" << sum << endl;
