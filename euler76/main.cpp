02018aac25e36b6dc6656a0f12ab14b490d247c4
hesu <hesuslee@gmail.com>
2016-08-16 23:59:46 +0900

    solved(need memoization) 	modified:   main.cpp

--- euler76/main.cpp
+++ euler76/main.cpp
@@ -9,9 +9,6 @@
 
 using namespace std;
 
-
-unsigned int memo[100][100] = {0};
-
 unsigned int coinsum( std::vector<int> coins, int val)
 {
   // c(n, s(m)) = c(n, s(m-1)) + c(n-m, s(m));
@@ -19,33 +16,9 @@ unsigned int coinsum( std::vector<int> coins, int val)
   if( val < 0 ) return 0;
   if( val == 0) return 1;
 
-  unsigned int a;
-
-  int idxa = coins[ coins.size() - 2];
-  if( memo[ idxa][ val] != 0) {
-    a = memo[ idxa][ val];
-  } else {
-    std::vector<int> c(coins);
-    c.pop_back();
-    a = coinsum( c, val);
-  }
-
-  unsigned int b;
-
-  int idxb = coins[ coins.size()-1];
-  if( memo[ idxb][ val - idxb] != 0) {
-    b = memo[ idxb][ val - idxb]; 
-  } else {
-    b = coinsum( coins, val - idxb);
-  }
-  
-  unsigned int ret = a + b;
-  if( memo[ idxb][ val] == 0) { 
-    cout << "coins.size()-1=" << idxb << " val=" << val << " for ret=" << ret << endl;
-    memo[ idxb][ val] = ret; 
-  }
-
-  return ret;
+  std::vector<int> c(coins);
+  c.pop_back();
+  return coinsum( c, val) + coinsum( coins, val - coins[ coins.size()-1]);
 }
 
 int main(int argc, char** argv)
