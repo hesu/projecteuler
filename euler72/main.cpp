c1d5800d4634d76f2405d25b08d63fab3e441a25
hesu <hesuslee@gmail.com>
2016-08-21 17:26:01 +0900

    on solving

--- euler72/main.cpp
+++ euler72/main.cpp
@@ -8,6 +8,8 @@
 #include <ctime>
 #include <vector>
 
+#include <math.h>
+
 using namespace std;
 
 class Fraction
@@ -23,28 +25,84 @@ class Fraction
   }
 };
 
+int findFirstTreeDepth( Fraction left, Fraction right, int maxd)
+{
+  int depth = 0;
+  Fraction ans = Fraction( left.n + right.n, left.d + right.d);
+
+  while( ans.d < maxd) {
+    if( depth %2 == 0) {
+      left.n = ans.n; left.d = ans.d;
+    } else {
+      right.n = ans.n; right.d = ans.d;
+    }
+
+    ans.n = left.n + right.n; ans.d = left.d + right.d;
+    if( ans.d > maxd) { break; }
+    depth++;
+  }
+
+  cout << "tree depth=" << depth << " (next) ans=" << ans.n << "/" << ans.d << endl;
+  return depth;
+}
+
 // using Stern-Brocot Tree
 unsigned int countingFractions( std::vector<Fraction> v, unsigned int maxd)
 {
-  int depth = 0;
+  unsigned int depth = 0;
   
+  std::vector<Fraction> thisturn_new;
   std::vector<Fraction> newv;
 
-while( depth < maxd) {
+  bool findmaxd = false;
+  while( !findmaxd) {
 
-  int vsize = v.size();
-  for(int i=0; i<vsize; i++)
-  {
-    if( i == 0 || i == vsize-1) {
+    int vsize = v.size();
+    for(int i=0; i<vsize; i++)
+    {
       newv.push_back( v[i]);
-    } else {
-      Fraction f( v[i].n + v[i+1].n , v[i].d + v[i+1].d);
-      newv.push_back(f);
+      
+      if( i < (vsize-1)) {
+        Fraction f( v[i].n + v[i+1].n , v[i].d + v[i+1].d);
+        thisturn_new.push_back(f);
+        newv.push_back(f);
+      }
+      
+    }
+
+  //  cout << "tree depth=" << depth << " thisturn_new.size()=" << thisturn_new.size() << endl;
+
+    bool all_over = true;
+    for(int i=0; i<thisturn_new.size(); i++) {
+      if( thisturn_new[i].d >= maxd) { 
+        cout << "found!" << endl; all_over = false; 
+        break; 
+      }
+    }
+    if( all_over == false) { findmaxd = true; continue;}
+
+    depth++;
+//    if( depth % 1000 == 0) { cout << "depth=" << depth << " thisturn_new.size() " << thisturn_new.size() << endl; }
+
+    v = newv;
+
+    newv.clear();
+    thisturn_new.clear();
+  }
+
+  cout << "depth=" << depth << " vector.size()=" << v.size() << endl;
+  
+  int sum=0;
+  for(int i=0; i<v.size(); i++)
+  {
+    if( v[i].d <= maxd) {
+      cout << v[i].n << "/" << v[i].d <<  " ";
+      sum++;
     }
   }
-  depth++;
-}
+  cout << endl;
 
+  return sum - 2;
 }
 
 int main(int argc, char** argv)
@@ -54,6 +112,7 @@ int main(int argc, char** argv)
   /* starting code */
 
   unsigned int maxd = 1000000;
+  //unsigned int maxd = 8;
  
   Fraction left(0,1);
   Fraction right(1,1);
@@ -62,8 +121,19 @@ int main(int argc, char** argv)
   f.push_back( left);
   f.push_back( right);
   
-  unsigned int howmany = countingFractions( f, maxd);
-  cout << "howmany=" << howmany << endl;
+//  unsigned int howmany = countingFractions( f, maxd);
+  //cout << "howmany=" << howmany << endl;
+
+  unsigned int depth = findFirstTreeDepth( left, right, maxd);
+  cout << "depth=" << depth << endl;
+
+
+  int sum=0;
+  for(int i=0; i<=depth; i++)
+  {
+    sum += pow( 2, i);
+  }
+  cout << "sum=" << sum << endl;
 
 	/* end of code */
 	clock_t end = clock();
