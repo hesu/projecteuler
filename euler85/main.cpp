9052274c7534f3ff6efe30e31de74d1b0d64bbb0
hesu <hesuslee@gmail.com>
2016-08-24 23:25:30 +0900

    solved

--- euler85/main.cpp
+++ euler85/main.cpp
@@ -7,7 +7,6 @@
 
 using namespace std;
 
-
 int countRectangles( int ex, int ey)
 {
   int sum = 0;
@@ -21,34 +20,38 @@ int countRectangles( int ex, int ey)
   return sum;
 }
 
+int abs( int n) { return( n>0 ? n : (-1)*n); }
+
+
 int main(int argc, char** argv)
 {
 	clock_t begin = clock();
 	/* starting code */
 
   int GOAL = 2000000;
-  int diff = GOAL;
 
-  int thatx = 1;
-  int thaty = 1;
+  int thatx=1;
+  int thaty=1;
+  int mindiff = GOAL;
 
-  bool done = false;
-  for(int x=1; !done; x++)
+  for(int x=1; x<= 1000; x++)
   {
-    for(int y=1; !done; y++)
+    if( x%100 == 0) { cout << "now x=" << x << endl; }
+    for(int y=1; y<=1000; y++)
     {
       int rects = countRectangles( x, y);
+      int d = abs( GOAL - rects);
 
-      int thisDiff = GOAL - rects;
-      if( thisDiff < 0) { // over
-        done = true;
-        break;
+      if( d < mindiff) {
+        mindiff = d;
+        thatx = x;
+        thaty = y;
       }
-
     }
   }
 
-
+  cout << "x=" << thatx << " y=" << thaty << " mindiff=" << mindiff << endl;
+  cout << "x*y=" << thatx*thaty << endl;
 	/* end of code */
 	clock_t end = clock();
 	std::cout << "elapsed time=" << double(end - begin) / CLOCKS_PER_SEC << std::endl;
