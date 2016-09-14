68c1672a72e5569f06346e13b6d525e280d889e5
hesu <hesuslee@gmail.com>
2016-09-14 12:17:34 +0900

    on solving

---
+++ euler89/main.cpp
@@ -0,0 +1,18 @@
+/*
+ 	Problem 89 - Roman numerals
+*/
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	clock_t begin = clock();
+  /* starting code */
+	/* end of code */
+	clock_t end = clock();
+	std::cout << "elapsed time=" << double(end - begin) / CLOCKS_PER_SEC << std::endl;
+	return 0;
+}
