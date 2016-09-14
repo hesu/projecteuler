3b19812f1da7c33513975cce40a0d3a7148c318f
hesu <hesuslee@gmail.com>
2016-09-14 11:20:10 +0900

    solved

--- euler102/main.cpp
+++ euler102/main.cpp
@@ -2,62 +2,45 @@
  	Problem 102 - Triangle containtment
 */
 
-#define _USE_MATH_DEFINES
-#include <math.h>
-	
 #include <iostream>
 #include <ctime>
-#include <cmath>
-
 #include <fstream>
 #include <vector>
 
 using namespace std;
 
-class Vector
+class Point 
 {
-  private:
-    float x;
-    float y;
   public:
-    Vector( float _x1, float _y1, float _x2, float _y2)
-    {
-      x = _x1 - _x2;
-      y = _y1 - _y2;
-    }
+  float x;
+  float y;
 
-    float getx() { return x;}
-    float gety() { return y;}
-    float getsize() { return sqrt( x*x + y*y); }
-
-    float getBetweenAngle( Vector v) 
-    { 
-      return acos( (x * v.getx() + y * v.gety()) / (getsize() * v.getsize()));
-    }
+  Point(float _x, float _y)
+  {
+    x = _x;
+    y = _y;
+  }
 };
 
-bool samecompare( float a, float b)
+// Is point(x1, y1) and point(x2,y2) are in same side divided by l1-l2 line plane?
+bool isSameSide( Point l1, Point l2, Point a, Point b)
 {
-  float diff= a-b;
-  if( diff < 0) diff *= -1;
-
-  if( diff < 0.00001) return true;
-  return false;
+  float sidea = ((l2.x - l1.x) * (a.y - l1.y) - ( l2.y - l1.y) * ( a.x - l1.x));
+  float sideb = ((l2.x - l1.x) * (b.y - l1.y) - ( l2.y - l1.y) * ( b.x - l1.x));
+  return (sidea * sideb > 0);
 }
 
 bool pointInTriangle( float xp, float yp, float x1, float y1, float x2, float y2, float x3, float y3)
 {
-  Vector v1(x1, y1, xp, yp);
-  Vector v2(x2, y2, xp, yp);
-  Vector v3(x3, y3, xp, yp);
+  Point a( x1, y1);
+  Point b( x2, y2);
+  Point c( x3, y3);
+  Point origin(0, 0);
 
-  float a1 = v1.getBetweenAngle( v2);
-  float a2 = v2.getBetweenAngle( v3);
-  float a3 = v3.getBetweenAngle( v1);
-  float theta = a1+a2+a3;
-//  cout << "a1=" << a1 << " a2=" << a2 << " a3=" << a3 << " theta=" << theta << " 2PI=" << 2*M_PI << endl;
+  if( isSameSide( a, b, origin, c) && isSameSide( b, c, origin, a) && isSameSide( a, c, origin, b)) {
+    return true;
+  }
 
-  if (samecompare((float)theta ,(float)2*M_PI)) return true;
   return false;
 }
 
@@ -70,13 +53,10 @@ int main(int argc, char** argv)
  // cout << pointInTriangle( 0, 0, -175, 41, -421, -714, 574, -645) << endl;
 
   int cnt = 0;
-
   ifstream inf( "p102_triangles.txt");
 
-  string save;
   string line;
   while( std::getline( inf, line)) {
-    save = line;
     std::vector<float> v;
 
     string delim = ",";
@@ -86,20 +66,12 @@ int main(int argc, char** argv)
     while(( pos = line.find( delim)) != std::string::npos) {
       string token = line.substr( 0, pos);
       line.erase( 0, pos + lenDelim);
-
       v.push_back(atof(token.c_str()));
     }
     v.push_back(atof(line.c_str()));
-
-    if( pointInTriangle( 0, 0, v[0], v[1], v[2], v[3], v[4], v[5])) {
-      cout << "line=" << save << " pointInTriangle" << endl;
-      cnt++;
-    } else {
-      cout << "line=" << save << endl;
-    }
+    if( pointInTriangle( 0, 0, v[0], v[1], v[2], v[3], v[4], v[5])) cnt++;
   }
   inf.close();
-
   cout << "cnt=" << cnt << endl;
 
 	/* end of code */
