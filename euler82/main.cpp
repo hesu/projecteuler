7cc33cbfbeecdcb4e7d538280138c56474734578
hesu <hesuslee@gmail.com>
2016-09-23 23:43:48 +0900

    on solving

--- euler82/main.cpp
+++ euler82/main.cpp
@@ -5,15 +5,43 @@
 #include <iostream>
 #include <ctime>
 #include <fstream>
+#include <vector>
 
 using namespace std;
+
+
+#define MATSIZE 5
+
+int getPathSum( int **mat, int col, int yrow, int sum)
+{
+  if (col <= 0) { return sum ; }
+
+
+  // 지금 col, yrow 에서
+  // 이전 col 의 어느 값까지의 최소경로를 찾는다.
+  int min = -1;
+
+  for(int r=0; r<MATSIZE; r++)
+  {
+    int minval = -1;
+    if( r == yrow) {
+      minval = mat[col-1][r] + mat[col][yrow];
+      cout << "found!" << endl;
+    }
+
+    if( min == -1) min = minval;
+    else if( minval > min) min = minval;
+  }
+ 
+  return getPathSum( mat, col-1, yrow, sum);
+}
 
 int main(int argc, char** argv)
 {
 	clock_t begin = clock();
 
-  int row=5; // 80;
-  int col=5; // 80;
+  int row=MATSIZE; // 80;
+  int col=MATSIZE; // 80;
 
   int **mat = (int**) malloc( sizeof( int*) * row);
   for(int i=0; i<row; i++)
@@ -47,15 +75,15 @@ int main(int argc, char** argv)
   }
   inf.close();
 
-/*
   cout << "MATRIX DEBUG!" << endl;
-  for(int y=0; y<row; y++)
-  {
-    for(int x=0; x<col; x++) cout << mat[y][x] << " ";
-    cout << endl;
-  }
-  */
+  for(int y=0; y<row; y++) { for(int x=0; x<col; x++) cout << mat[y][x] << " "; cout << endl; }
 
+  std::vector<int> pathsums;
+
+  int yrow = 0;
+  int ps = getPathSum( mat, col, yrow, 0);
+  cout << "ps=" << ps << endl;
+/*
   for(int left=0; left<row; left++)
   {
     for(int right=0; right<row; right++)
@@ -75,6 +103,7 @@ int main(int argc, char** argv)
       free(memo);
     }
   }
+  */
 
 	clock_t end = clock();
 	std::cout << "elapsed=" << double( end-begin) / CLOCKS_PER_SEC << endl;
