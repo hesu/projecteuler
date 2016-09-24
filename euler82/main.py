c5899e42c6cd29156643ea3c5236206cd7421e5b
hesu <hesuslee@gmail.com>
2016-09-24 16:03:33 +0900

    can't solve big problem due to recursion depth

--- euler82/main.py
+++ euler82/main.py
@@ -1,3 +1,6 @@
+import copy
+import sys
+
 class Vertex :
   def __init__(self, row=0, col=0, val=0) :
     self.row = row
@@ -15,7 +18,7 @@ def setVertexNeighbor( vmat, row, col):
   if v.up is None and row > 0:
     v.up = vmat[ row-1][col]
   if v.down is None and row < len(vmat)-1:
-    v.up = vmat[ row+1][col]
+    v.down = vmat[ row+1][col]
   if v.right is None and col < len(vmat)-1:
     v.right = vmat[ row][col+1]
 
@@ -30,20 +33,49 @@ def setVertexMatrix( mat, vmat):
   for row in range( len(vmat)):
     for col in range( len(vmat[row])):
       setVertexNeighbor( vmat, row, col)
+  
   return vmat
 
-def traverse( vertex, vmat, record):
+def traverse( vertex, vmat, record, traversum):
   if record is None:
     record = [ (vertex.row, vertex.col)]
-  print(record)
+
+  if vertex.right is None :
+    # END condition
+    return traversum + vmat[ vertex.row][vertex.col].val
+
+  upval = sys.maxint
+  if vertex.up is not None :
+    if (vertex.row-1, vertex.col) not in record :
+      ur = copy.deepcopy( record)
+      ur.append( ( vertex.row, vertex.col))
+      upval = traversum + traverse( vmat[ vertex.row-1][vertex.col], vmat, ur, vertex.val)
+
+  downval = sys.maxint
+  if vertex.down is not None :
+    if (vertex.row+1, vertex.col) not in record :
+      dr = copy.deepcopy( record)
+      dr.append( (vertex.row, vertex.col))
+      downval = traversum + traverse( vmat[ vertex.row+1][vertex.col], vmat, dr, vertex.val)
+  
+  rightval = sys.maxint
+  if vertex.right is not None :
+    rr = copy.deepcopy( record)
+    rightval = traversum + traverse( vmat[ vertex.row][vertex.col+1], vmat, rr, vertex.val)
+
+  print( "REACHED!!!!!")
+  return min( upval, downval, rightval)
 
 read = None
-with open( "p082_matrix.small.txt") as f:
+with open( "p082_matrix.txt") as f:
   matrix = parse( f)
-  print( matrix)
+#  print( matrix)
   vmatrix = setVertexMatrix( matrix, None)
 
-  print( matrix[1][0])
-  minval = traverse(vmatrix[1][0], vmatrix, None)
+  minvals = [] 
+  for i in range( len(vmatrix)) :
+    minvals.append( traverse( vmatrix[i][0], vmatrix, None, 0))
+  
+  print( "minval=", min(minvals))
 #  minval = traverse( vmatrix[
 #print( vmatrix)
