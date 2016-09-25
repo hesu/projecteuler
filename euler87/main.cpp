e3b252c159220c25b178384e5ff17384c5e3d222
hesu <hesuslee@gmail.com>
2016-09-25 15:23:13 +0900

    start solving

---
+++ euler87/main.cpp
@@ -0,0 +1,20 @@
+/*
+  Problem 87 - Prime power triples
+*/
+
+#include <iostream>
+#include <ctime>
+#include <vector>
+
+#define LIMIT 50000000
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  clock_t begin = clock();
+
+
+  clock_t end = clock();
+  std::cout << "elapsed time=" << double( end-begin) / CLOCKS_PER_SEC << endl;
+}
