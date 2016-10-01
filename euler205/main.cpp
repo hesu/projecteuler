00c67308120a1f6ab411552d7055b8f160859429
hesu <hesuslee@gmail.com>
2016-10-01 17:59:02 +0900

    start solving

---
+++ euler205/main.cpp
@@ -0,0 +1,16 @@
+/*
+  Problem 205 - Dice Game
+*/
+
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  clock_t begin = clock();
+  clock_t end = clock();
+  std::cout << "elapsed time=" << double( end-begin) / CLOCKS_PER_SEC << endl;
+}
