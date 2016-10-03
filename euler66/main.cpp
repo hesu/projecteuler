2122b6c89a6bc9e83bf5b7d116b45708c02ee534
hesu <hesuslee@gmail.com>
2016-10-03 19:50:36 +0900

    retry solving

--- euler66/main.cpp
+++ euler66/main.cpp
@@ -1,47 +1,21 @@
 /*
- 	Problem 66 - Diophantine equation
+  Problem 66 - Diophantine equation
 */
-	
+
 #include <iostream>
 #include <ctime>
 #include <vector>
 #include <cmath>
-using namespace std;
+#include <algorithm>
 
-int getMinX( int d)
-{
-	for(int i=1;;i++)
-	{
-		double sqrted = sqrt( pow(i,2)* d + 1);
-		if( sqrted == ceil(sqrted)) {
-//			cout << "D=" << d << " y=" << i << " x=" << sqrted << endl;
-			return sqrted;
-		}
-	}
-}
+#include "BigInt.h"
+
+using namespace std;
 
 int main(int argc, char** argv)
 {
-	clock_t begin = clock();
-	/* starting code */
+  clock_t begin = clock();
 
-	int MAXD = 1000;
-
-	int xmax = 0;
-	for(int d=2; d<=MAXD; d++)
-	{
-		double sqrted = sqrt( d);
-		if( sqrted == ceil( sqrted)) { continue; }
-
-		int minX = getMinX(d);
-		if (minX > xmax) {
-			xmax = minX;
-			cout << "xmax = " << xmax << " D=" << d << endl;
-		}
-	}
-
-	/* end of code */
-	clock_t end = clock();
-	std::cout << "elapsed time=" << double(end - begin) / CLOCKS_PER_SEC << std::endl;
-	return 0;
+  clock_t end = clock();
+  std::cout << "elapsed time=" << double( end-begin) / CLOCKS_PER_SEC << endl;
 }
