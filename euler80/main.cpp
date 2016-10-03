50418aa788054a0b5d6105b33bdd7dbca491020f
hesu <hesuslee@gmail.com>
2016-10-03 17:46:05 +0900

    on solving

--- euler80/main.cpp
+++ euler80/main.cpp
@@ -14,32 +14,28 @@ using namespace std;
 
 #define MAX 100
 
-class AfterDecimal
-{
-  public:
-    AfterDecimal( int _e, int _n)
-    {
-      e = _e; n = _n;
-    }
-
-  private:
-  int e; // minus
-  int n;
-};
-
 std::vector<int> digital_expansion( int num, int max)
 {
   std::vector<int> v;
-  
+ 
+
   std::vector<int> input;
   input.push_back(num);
   for(int i=0;i<MAX;i++) input.push_back(0);
-
-  BigInt n(input);
-  n.print();
-//  cout << n.toString() << endl;
-
   
+  BigInt n(input);
+
+  // Get First Integer!
+  int fi;
+  for(fi=0; fi<num; fi++) if( pow(fi+1, 2) > num) break;
+
+  std::vector<int> comp_vector;
+  for(int i=0; i<MAX/2; i++) comp_vector.push_back(0);
+  comp_vector.push_back( fi);
+
+  BigInt comp( comp_vector);
+
+//  cout << "n length=" << n.getDigitsLen() << " comp len=" << comp.getDigitsLen() << endl;
 
   return v;
 }
