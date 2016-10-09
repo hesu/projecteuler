3b19760b36042fc4f163a3cceade39d54b0f2010
hesu <hesuslee@gmail.com>
2016-10-09 16:58:17 +0900

    on solving

--- euler83/main.cpp
+++ euler83/main.cpp
@@ -6,6 +6,7 @@
 #include <ctime>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -14,58 +15,138 @@ int minOf( vector<int> v)
   int min = v[0];
   for(int i=0;i<v.size();i++) {
     if( v[i] == -1) continue;
+    if( min == -1) min = v[i];
     if( v[i] < min) min = v[i];
   }
   return min;
 }
 
-int getMinPathValue( int nowrow, int nowcol, int row, int col, int **mat, int** memo, int matsize)
+bool adjacent( pair<int,int> a, pair<int,int> b)
 {
-//  cout << "getMinPathValue() row=" << row << " col=" << col << " val=" << mat[row][col] << endl;
+  if( a.first == b.first) {
+    if (a.second + 1 == b.second) return true;
+    if (a.second - 1 == b.second) return true;
+  }
+
+  if( a.second == b.second) {
+    if (a.first+ 1 == b.first) return true;
+    if (a.first- 1 == b.first) return true;
+  }
+  return false;
+}
+
+int value( int** mat, int size, pair<int,int> a)
+{
+  // first : y(row), second : x(col)
+  return mat[ a.first][a.second];
+}
+
+void printpath( vector<pair<int,int>> path, int** mat)
+{
+  for(int i=path.size()-1; i>=0; i--)
+  {
+    cout << "[" << mat[path[i].first][path[i].second] << "]";
+    if( i > 0) { cout << "-"; }
+  }
+  cout << endl;
+}
+
+bool inbound( pair<int,int> a, int matsize)
+{
+  if( a.first >= 0 && a.first <= matsize-1 && a.second >= 0 && a.second <= matsize-1){
+    //cout << "inbound true x=" << a.second << " y=" << a.first << endl;
+    return true;
+  }
+    //cout << "inbound false x=" << a.second << " y=" << a.first << endl;
+  return false;
+}
+
+bool need_discovery( vector<pair<int,int>> visited, pair<int,int> p)
+{
+  if( find( visited.begin(), visited.end(), p) == visited.end()) return true;
+  return false;
+}
+
+int getMinPathValue( pair<int,int> now, pair<int,int> goal, int **mat, int** memo, int matsize, vector<pair<int,int>> visited)
+{
+  if(adjacent( now, goal)) {
+    int retval = value( mat, matsize, now) + value( mat, matsize, goal);
+  //  cout << "adjacent! nowx=" << now.second << " nowy=" << now.first << " visited=" << visited.size() << " retval=" << retval << endl;
+    return retval;
+  } else {
+   // cout << "nowx=" << now.second << " nowy=" << now.first << " visited=" << visited.size() << endl;
+  }
+
+  int northroot = -1, southroot = -1, eastroot = -1, westroot = -1;
+
+  pair<int,int> n = make_pair(now.first-1, now.second);
+  pair<int,int> s = make_pair(now.first+1, now.second);
+  pair<int,int> e = make_pair(now.first, now.second+1);
+  pair<int,int> w = make_pair(now.first, now.second-1);
+
   
-  // 종료조건
-  if( nowrow == row && nowcol + 1 == col) {
-    return mat[nowrow][nowcol] + mat[row][col];
+  std::vector<pair<int,int>> visited_north( visited);
+  std::vector<pair<int,int>> visited_south( visited);
+  std::vector<pair<int,int>> visited_east( visited);
+  std::vector<pair<int,int>> visited_west( visited);
+  
+  // north discover;
+  if( inbound( n, matsize) && need_discovery( visited, n)) {
+    visited_north.push_back( n);
+    //cout << "call north " << endl;
+    northroot = getMinPathValue( n, goal, mat, memo, matsize, visited_north);
   }
   
-  if( nowrow == row && nowcol - 1 == col) {
-    return mat[nowrow][nowcol] + mat[row][col];
-  }
-
-  if( nowrow + 1 == row && nowcol == col) {
-    return mat[nowrow][nowcol] + mat[row][col];
-  }
-  
-  if( nowrow - 1 == row && nowcol == col) {
-    return mat[nowrow][nowcol] + mat[row][col];
-  }
-
-
-  // 점화식
-  int north = -1, west = -1, east = -1, south = -1;
-  // get north
-  if( row > 0) {
-    north = getMinPathValue( nowrow, nowcol, row-1, col, mat, memo, matsize);
-  }
-
-  // get south
-  if( row < matsize-1) {
-   south = getMinPathValue( nowrow, nowcol, row+1, col, mat, memo, matsize);
-  }
-
-  // get west
-  if( col > 0) {
-    west = getMinPathValue( nowrow, nowcol, row, col-1, mat, memo, matsize);
+  // south discover;
+  if( inbound( s, matsize) && need_discovery( visited, s)) {
+    visited_south.push_back( s);
+    //cout << "call south" << endl;
+    southroot = getMinPathValue( s, goal, mat, memo, matsize, visited_south);
   }
   
-  // get east 
-  if( col < matsize-1) {
-    east = getMinPathValue( nowrow, nowcol, row, col+1, mat, memo, matsize);
+  // east discover;
+  if( inbound( e, matsize) && need_discovery( visited, e)) {
+    visited_east.push_back( e);
+    //cout << "call east" << endl;
+    eastroot = getMinPathValue( e, goal, mat, memo, matsize, visited_east);
+  }
+  
+  // west discover;
+  if( inbound( w, matsize) && need_discovery( visited, w)) {
+    visited_west.push_back( w);
+    //cout << "call west" << endl;
+    westroot = getMinPathValue( w, goal, mat, memo, matsize, visited_west);
   }
 
-  vector<int> v = { north, west, east, south };
-  int min = minOf( v );
-  return mat[row][col] + min;
+  //cout << "n=" << northroot << " s=" << southroot << " e=" << eastroot << " w=" << westroot << endl;
+  std::vector<int> minroot = { northroot, southroot, eastroot, westroot };
+
+  if( minOf(minroot) == -1) { 
+  //   cout << "unexpected case at (" << now.second << "," << now.first << ")" << endl; 
+//     return -1;
+  }
+
+  int retval = minOf(minroot) + value(mat, matsize, now);
+  cout << "minval at (" << now.second << "," << now.first << ")=" << retval << endl; 
+
+  if( minOf(minroot) == northroot) {
+//    cout << "north root" << endl;
+    printpath( visited_north, mat);
+  }
+  if( minOf(minroot) == southroot) {
+ //   cout << "south root" << endl;
+    printpath( visited_south, mat);
+  }
+  if( minOf(minroot) == westroot ) {
+  //  cout << "west root" << endl;
+    printpath( visited_west, mat);
+  }
+  if( minOf(minroot) == eastroot) {
+   // cout << "east root" << endl;
+    printpath( visited_east, mat);
+  }
+
+  return retval;
 }
 
 int main(int argc, char** argv)
@@ -134,7 +215,14 @@ int main(int argc, char** argv)
     memo[i] = row;
   }
 
-  int min = getMinPathValue( 0, 0, row-1, col-1, mat, memo, matsize);
+  pair<int,int> current = make_pair( row-1, col-1);
+  pair<int,int> goal = make_pair( 0, 0);
+
+  vector<pair<int,int>> visited;
+  visited.push_back( current);
+
+  char from = '0';
+  int min = getMinPathValue( current, goal, mat, memo, matsize, visited);
   cout << "min=" << min << endl;
 
 	clock_t end = clock();
