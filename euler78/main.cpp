292ad5a9aed27f1a5d78e676ffdb23445446c980
hesu <hesuslee@gmail.com>
2016-10-30 04:19:16 +0900

    on solving 78

--- euler78/main.cpp
+++ euler78/main.cpp
@@ -13,40 +13,68 @@ using namespace std;
 
 #define MAX 1000000
 
-long long int waysfor( long long int n, long long int maxd)
-{
-//  cout << "waysfor n=" << n << " maxd=" << maxd << endl;
-  if( maxd <= 0) return 0;
-  if( n == 0) return 1;
-  if( n < 0) return 0;
-  return waysfor(n, maxd-1) + waysfor( n-maxd, maxd);
-}
+bool max_overed = true;
 
-map<pair<long long int,long long int>, long long int> waysmap;
+map<long long int, long long int> waysmap;
+
+long long int pentagonal_number( long long int n)
+{
+  return (n * ( 3*n - 1) / 2);
+}
 
 long long int coin_partition( long long int n)
 {
-  // TODO
-  long long int sum=0;
-  for(long long int i=n; i>=1; i--)
-  {
-    if( i == 1 || i == n) sum++;
-    else {
-      pair<long long int,long long int> wf = make_pair( n-i, i);
-      map<pair<long long int,long long int>,long long int>::iterator it = waysmap.find( wf);
-      long long int val;
+  if( n == 0) return 1;
+  if( n == 1) return 1;
 
-      if( it != waysmap.end()) {
-        val = it->second;
-        cout << "find second : " << val << endl;
-      } else {
-        val = waysfor(n-i, i);
-        waysmap.insert( map<pair<long long int,long long int>,long long int>::value_type( wf, val));
-      }
-      sum += waysfor( n-i, i);
-    }
-  }
+  long long int sum = 0;
+  int sign = 1;
   
+  map<long long int, long long int>::iterator it;
+
+  for(int i=1;i<n;i++)
+  {
+    int sign;
+    if( i%4 > 1) { sign = -1; } else {sign = 1; }
+
+    int each = pentagonal_number( n - i);
+    if( each < 0) break;
+    int val;
+      it = waysmap.find( each);
+      if( it == waysmap.end()) {
+        val =  sign * coin_partition(n-each);
+      } else{
+        val = it->second;
+      }
+
+    if( val < 0) break;
+    else sum += val;
+
+    int each2 = pentagonal_number( n + i);
+    if( each2 < 0) break;
+    int val2;
+      it = waysmap.find( each2);
+      if( it == waysmap.end()) {
+        val2 =  sign * coin_partition(n-each2);
+      } else{
+        val2 = it->second;
+      }
+
+    if( val2 < 0) break;
+    else sum += val2;
+
+  }
+
+  if( max_overed) {
+    sum = sum % ( MAX * 10);
+  }
+
+  it = waysmap.find( n);
+  if (it == waysmap.end()) {
+    waysmap.insert( map<long long int, long long int>::value_type( n, sum));
+  }
+//  cout << "sum(" << sum << ") for n=" << n << endl;
+
   return sum;
 }
 
@@ -54,33 +82,23 @@ int main(int argc, char** argv)
 {
   clock_t begin = clock();
 
-/*
-  cout << "waysfor(4, 2) = " << waysfor( 4, 2) << endl;
-  cout << "waysfor(3, 2) = " << waysfor( 3, 2) << endl;
-  
-  cout << "waysfor(4, 1) = " << waysfor( 4, 1) << endl;
-  cout << "waysfor(3, 1) = " << waysfor( 3, 1) << endl;
-  cout << "waysfor(2, 1) = " << waysfor( 2, 1) << endl;
-
-  cout << "coinpart t(1) = " << coin_partition(1) << endl;
-  cout << "coinpart t(2) = " << coin_partition(2) << endl;
-  cout << "coinpart t(3) = " << coin_partition(3) << endl;
-  cout << "coinpart t(4) = " << coin_partition(4) << endl;
-  cout << "coinpart t(5) = " << coin_partition(5) << endl;
-  */
-
-long long int sol=1;
+  cout << "cp(1)=" << coin_partition(1) << endl;
+  cout << "cp(2)=" << coin_partition(2) << endl;
+  cout << "cp(3)=" << coin_partition(3) << endl;
+  cout << "cp(4)=" << coin_partition(4) << endl;
+  cout << "cp(5)=" << coin_partition(5) << endl;
+  cout << "cp(6)=" << coin_partition(6) << endl;
+ 
+ /*
+  long long int sol=1;
   while(true)
   {
-    //if( sol % 10000 == 0) { cout << "now n=" << sol << endl; }
     long long int par = coin_partition( sol);
-    if( par<0) { cout << "overflow. break" << endl; break; }
-    cout << "now n=" << sol << " << partition=" << par << endl;
-    if( par >= MAX && par % MAX == 0) break;
+    if( max_overed && par % MAX == 0) break;
     sol++;
   }
-
   cout << "sol=" << sol << endl;
+  */
 
   clock_t end = clock();
   std::cout << "elapsed time=" << double( end-begin) / CLOCKS_PER_SEC << endl;
