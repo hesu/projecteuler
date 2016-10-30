226fece860db1099110731173281593baeed5862
hesu <hesuslee@gmail.com>
2016-10-30 13:11:03 +0900

    start solving 100

---
+++ euler100/main.cpp
@@ -0,0 +1,22 @@
+/*
+ 	Problem 100 - Arranged probability
+*/
+
+#include <iostream>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	clock_t begin = clock();
+
+	/* starting code */
+	/* end of code */
+	
+	clock_t end = clock();
+	std::cout << "elapsed time=" << double(end - begin) / CLOCKS_PER_SEC << std::endl;
+	return 0;
+}
