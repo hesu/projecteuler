c01313dd94f983faf38612275c8c9d0c56de2d30
hesu <hesuslee@gmail.com>
2017-10-08 18:05:30 +0900

    solving 107

--- euler107/main.cpp
+++ euler107/main.cpp
@@ -8,9 +8,32 @@
 #include <map>
 #include <fstream>
 #include <sstream>
+#include <algorithm>
 using namespace std;
 
-// vertex : int id, map<int vertex, int weight>
+class Edge
+{
+  public :
+  Edge(int _cost, int _a, int _b)
+  {
+    cost = _cost;
+    a = _a;
+    b = _b;
+    reducible = false;
+  }
+
+  int getCost() const { return cost; }
+  int getA() { return a; }
+  int getB() { return b; }
+  bool getReducible() const { return reducible; }
+  void setReducible(bool b) { reducible = b; }
+
+  private :
+  int cost;
+  int a;
+  int b;
+  bool reducible;
+};
 
 class Vertex
 {
@@ -29,6 +52,27 @@ class Vertex
     m.insert(map<int,int>::value_type(otherId, weight));
   }
 
+  void unconnect(int otherId)
+  {
+    m.erase(otherId);
+  }
+
+  bool isConnected(int otherId)
+  {
+    auto iter = m.find(otherId);
+    if (iter != m.end()) { return true; }
+    return false;
+  }
+
+  int getCost(int otherId)
+  {
+    if (id == otherId) return 0;
+    
+    auto iter = m.find(otherId);
+    if (iter == m.end()) return -1;
+    return iter->second;
+  }
+
   int weightSum()
   {
     int sum = 0;
@@ -39,12 +83,25 @@ class Vertex
     return sum;
   }
 };
+  
+bool sortEdges( const Edge &lhs, const Edge &rhs)
+{
+  return (lhs.getCost() > rhs.getCost());
+}
 
 class Network
 {
+  // TODO : 
+  // a. edge 가 cost 순서로 정렬된 자료구조 만들기
+  // b. edge 제거 알고리즘 짜기
+
   public :
+  Network()
+  {
+    reducedNetwork = false;
+  }
   
-  Network(string fname)
+  void init(string fname)
   {
     ifstream inf(fname);
     string line;
@@ -65,35 +122,144 @@ class Network
         otherId++;
         if (token != "-")
         {
-          v.connect( otherId, stoi(token));
+          int cost = stoi(token);
+          v.connect( otherId, cost);
+
+          if (linecnt < otherId)
+          {
+            Edge e(cost, linecnt, otherId);
+            edges.push_back(e);
+          }
         }
       }
 
-      vertices.push_back(v);
+      vertices.insert(map<int,Vertex>::value_type(linecnt, v));
     }
 
     inf.close();
+
+    sort(edges.begin(), edges.end(), sortEdges);
+    for(int i=0; i<edges.size(); i++) { cout << "[" << i << "] cost=" << edges[i].getCost() << endl; }
   }
 
   int weightSum()
   {
     int sum = 0;
-    for(int i=0; i<vertices.size(); i++)
+    for(int i=0; i<edges.size(); i++)
     {
-      sum += vertices[i].weightSum();
+      if (edges[i].getReducible() == false)
+      {
+        sum += edges[i].getCost();
+      }
     }
-    return (sum/2);
+    return sum;
+  }
+  
+  bool isConnectable(int id, int otherId, vector<int> notSearch)
+  {
+    cout << "\tisConnectable check : id=" << id << " other=" << otherId << endl;
+    if(id == otherId) 
+    {
+      return true;
+    }
+
+    auto va = vertices.find(id);
+    if (va == vertices.end()) {
+      cout << "\tnot have " << id << endl;
+      return false;
+    }
+
+    if (va->second.isConnected(otherId))
+    {
+      cout << "\t connected. id=" << id << " otherId=" << otherId << " (cost=" << va->second.getCost(otherId) << ")" << " notSearch.size=" << notSearch.size() << endl;
+      for(int i=0; i<notSearch.size(); i++)
+      {
+        cout << "[" << notSearch[i] << "]-";
+      }
+      cout << "[" << id << "]-[" << otherId << "]" << endl;
+      return true;
+    }
+
+    notSearch.push_back(id);
+      
+    for(; va != vertices.end(); va++)
+    {
+      int thisId = va->first;
+      if(find(notSearch.begin(), notSearch.end(), va->first) != notSearch.end())
+      {
+        cout << "\t continue;" << endl;
+        continue;
+      }
+
+      bool is = isConnectable(va->first, otherId, notSearch);
+      if (is) return true;
+    }
+    return false;
   }
 
   void reduceWeight()
   {
-    /////////
-    // TODO
-    /////////
+    // 가장 weight 가 높은 edge 부터 하나씩 지워 나간다.
+    bool done = false;
+    while(!done)
+    {
+      bool reduced = false;
+      for(int i=0; i<edges.size(); i++)
+      {
+        cout << "\tedges[" << i << "]" << endl;
+        if (reducedNetwork) break;
+
+        int a = edges[i].getA();
+        int b = edges[i].getB();
+
+        auto va = vertices.find(a);
+        if (va != vertices.end())
+        {
+          vector<int> notSearch;
+
+          // remove edge from vertices
+          int aCost = va->second.getCost(b);
+          va->second.unconnect(b);
+
+          auto vb = vertices.find(b);
+          int bCost = va->second.getCost(a);
+          vb->second.unconnect(a);
+       
+          if (isConnectable(a, b, notSearch))
+          {
+            cout << "Remove " << a << ", " << b << " cost(" << edges[i].getCost() << ")" << endl;
+            // remove edge from vertex
+            va->second.unconnect(b);
+            auto vb = vertices.find(a);
+            if (vb != vertices.end())
+            {
+              vb->second.unconnect(a);
+            }
+
+            // update edge
+            edges[i].setReducible(true);
+            reduced = true;
+          }
+          else
+          {
+            // restore edge to vertices
+            va->second.connect(b, aCost);
+            vb->second.connect(a, bCost);
+          }
+        }
+      }
+
+    //  done = true;
+
+      if (!reduced) { done = true; }
+    }
+
   }
 
   private :
-    vector<Vertex> vertices;
+    map<int, Vertex> vertices;
+    vector<Edge> edges;
+    bool reducedNetwork;
 };
 
 int main(int argc, char** argv)
@@ -101,9 +267,15 @@ int main(int argc, char** argv)
 	clock_t begin = clock();
 
 	/* starting code */
-  Network n("p107_network.small.txt");
+  Network n;
+  n.init("p107_network.small.txt");
+
   int sum = n.weightSum();
   cout << "sum=" << sum << endl;
+
+  n.reduceWeight();
+  int reducedSum = n.weightSum();
+  cout << "reduced sum=" << reducedSum << " (saving " << sum - reducedSum << ")" << endl;
 
 	/* end of code */
 	clock_t end = clock();
