7e190875c18447fbc552ed279e75f193aa30af06
hesu <hesuslee@gmail.com>
2018-09-23 14:12:53 +0900

    modified:   main.cpp

--- euler100/main.cpp
+++ euler100/main.cpp
@@ -9,11 +9,42 @@
 
 using namespace std;
 
+#define BIG 1000000000000
+
 int main(int argc, char** argv)
 {
 	clock_t begin = clock();
 
 	/* starting code */
+  unsigned int a = 1;
+  unsigned int b = 1;
+  bool done = false;
+
+  while(done != true)
+  {
+    a++;
+    while(true)
+    {
+      unsigned int lvalue = 2 * a * (a-1);
+      unsigned int rvalue = (a+b) * (a+b-1);
+
+      if (lvalue == rvalue)
+      {
+        if (a+b >= BIG)
+          done = true;
+
+        cout << "a=" << a << " , b=" << b << endl;
+        break;
+      }
+
+      if (lvalue > rvalue)
+        b++;
+      else
+        a++;
+    }
+  }
+
+  cout << "LAST a=" << a << " , b=" << b << endl;
 	/* end of code */
 	
 	clock_t end = clock();
