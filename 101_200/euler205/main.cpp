82cc3326b9681e381991e97acf5aa88f46aa3f6f
hesu <hesuslee@gmail.com>
2020-11-27 23:33:31 +0900

    solved 205 	수정됨:       main.cpp

--- 101_200/euler205/main.cpp
+++ 101_200/euler205/main.cpp
@@ -8,252 +8,112 @@
 #include <sstream>
 #include <cmath>
 #include <vector>
-#include <algorithm>
+#include <map>
 
 using namespace std;
 
-class poly
+vector<unsigned int> get_next_case(const vector<unsigned int>& before_case, const vector<unsigned int>& dice)
 {
-  public:
-    poly(int _c, int _e)
-    {
-      c = _c; e = _e;
-    }
-    const int getc() { return c;}
-    const int gete() { return e;}
-    void setc( int _c) { c = _c; }
-    string getString()
-    {
-      stringstream ss;
-      ss << c;
-      ss << "*x^";
-      ss << e;
-      return ss.str();
-    }
-  private:
-    int c; // coefficient(계수)
-    int e; // exponent(지수)
-};
+  vector<unsigned int> v;
 
-bool polySort( poly a, poly b)
-{
-  return (a.gete() < b.gete());
-}
-
-class polynomials
-{
-  public:
-    polynomials() {}
-
-    std::vector<poly> get() { return p; }
-    
-    polynomials( std::vector<poly> _p)
-    {
-      p = _p;
-    }
-
-    polynomials& operator +=(poly po)
-    {
-      for(int i=0; i<p.size(); i++)
-      {
-        if( p[i].gete() == po.gete()) {
-          p[i].setc( p[i].getc() + po.getc());
-          return *this;
-        }
-      }
-      p.push_back(po);
-      return *this;
-    }
-
-    polynomials operator *(polynomials &rhs)
-    {
-      std::vector<poly> v;
-      polynomials that(v);
-
-      std::vector<poly> rhsv = rhs.get();
-
-      if (p.size() == 0) {
-        return polynomials( rhsv);
-      } else {
-        for(int i=0; i<p.size(); i++)
-        {
-          for(int j=0; j<rhsv.size(); j++)
-          {
-            poly mul( p[i].getc() * rhsv[j].getc(), p[i].gete() + rhsv[j].gete());
-            that += mul;
-          }
-        }
-      }
-
-      return that;
-    }
-
-    void sort()
-    {
-      std::sort( p.begin(), p.end(), polySort);
-    }
-
-    polynomials pow( int n)
-    {
-      polynomials powed;
-
-      polynomials mul = polynomials(p);
-      for(int i=0; i<n; i++)
-      {
-        powed = powed * mul;
-      }
-
-      powed.sort();
-
-      return powed;
-    }
-
-    string getString()
-    {
-      string ret;
-      for(int i=0; i<p.size(); i++)
-      {
-        ret += p[i].getString();
-        if( i < p.size()-1) {
-          ret += " + ";
-        }
-      }
-      return ret;
-    }
-
-    int coefficientsum()
-    {
-      int cosum = 0;
-      for(int i=0;i<p.size(); i++){ cosum += p[i].getc(); }
-      return cosum;
-    }
-
-  private:
-    std::vector<poly> p;
-};
-
-int gcd( int x, int y)
-{
-  int s; // smaller
-  int b; // bigger
-
-  if( x == y) return x;
-
-  if( x > y) { b = x; s = y; }
-  else { b = y; s = x; }
-
-  for(int i=s; i>=1; i--)
+  for(int i=0; i < before_case.size(); i++)
   {
-    if ((b%i == 0) && (s%i == 0)) {
-      return i;
-    }
-  }
-  return 1;
-}
-
-class Fraction
-{
-  public :
-
-  Fraction() {}
-  Fraction( int _n, int _d) { n = _n; d = _d; }
-
-  Fraction reduce()
-  {
-      int gcd_this = gcd( d, n);
-      int _n = n/ gcd_this;
-      int _d = d/ gcd_this;
-      Fraction f = Fraction( _n, _d);
-      return f;
-  }
-
-  Fraction operator * (Fraction rhs)
-  {
-   Fraction f;
-   f.n = n * rhs.d + rhs.n + d;
-   f.d = d * rhs.d;
-   return f.reduce();
-  }
-  
-  Fraction operator += (Fraction rhs)
-  {
-   Fraction f;
-   f.n = n * rhs.d + d * rhs.n;
-   f.d = d * rhs.d;
-   return f.reduce();
-  }
-
-  int n;
-  int d;
-};
-
-float me_win_probability( polynomials dice_me, polynomials dice_you)
-{
-  // win probability for my dice.
-  // return (whole_prob, me_win_prob)
-  unsigned int sum = 0;
-  unsigned int all = pow(4,9) * pow(6,6);
-
-  std::vector<poly> myv = dice_me.get();
-  std::vector<poly> youv = dice_you.get();
-
-  unsigned int sum_lose_or_tie = 0;
-
-  int my_dice_max = myv[ myv.size() -1].gete();
-  int you_dice_max = youv[ youv.size() -1].gete();
-
-  cout << "my_dice_max=" << my_dice_max << " you_dice_max=" << you_dice_max << endl;
-  
-  for(int i=0; i<myv.size(); i++)
-  {
-    for(int j=0; j<youv.size(); j++)
+    for(int j=0; j < dice.size(); j++)
     {
-      unsigned int myc = myv[i].getc();
-      unsigned int youc = youv[j].getc();
-
-      if( myv[i].gete() > youv[j].gete()) {
-        sum += myc*youc;
-      } else {
-        sum_lose_or_tie += myc * youc;
-      }
+      v.push_back(before_case[i] + dice[j]);
     }
   }
 
-  cout << " sum_win=" << sum << " sum_lose_or_tie=" << sum_lose_or_tie << " all=" << all << " win+lose+tie=" << sum + sum_lose_or_tie << endl;
-
-  cout << "myv.size()=" << myv.size() << " youv.size()=" << youv.size() << endl;
-  cout << "all=" << all << endl;
-  
-  float f = (float)sum/(float)all;
-  cout << setprecision(7) << "probability =" << f << endl;
-
-  return f;
+  return v;
 }
 
 int main(int argc, char** argv)
 {
   clock_t begin = clock();
 
-  std::vector<poly> pv;
-  for(int i=1; i<=4; i++) {
-    pv.push_back( poly(1, i));
-  }
-  polynomials pdice( pv);
-  polynomials pdice_rolled = pdice.pow(9);
-  cout << pdice_rolled.getString() << endl;
-  
-  cout << endl;
- 
-  std::vector<poly> cv;
-  for(int i=1; i<=6; i++) {
-    cv.push_back( poly(1, i));
-  }
-  polynomials cdice( cv);
-  polynomials cdice_rolled = cdice.pow(6);
-  cout << cdice_rolled.getString() << endl;
+  unsigned long long int whole = pow(4,9) * pow(6,6);
+  cout << "whole=" << whole << endl;
 
-  float winprob = me_win_probability( pdice_rolled, cdice_rolled);
-  cout << "winprob=" << winprob << endl;
+  vector<unsigned int> pydice = {1,2,3,4};
+  vector<unsigned int> cudice = {1,2,3,4,5,6};
+
+  vector<unsigned int> p2 = get_next_case(pydice, pydice);
+  vector<unsigned int> p3 = get_next_case(p2, pydice);
+  vector<unsigned int> p4 = get_next_case(p3, pydice);
+  vector<unsigned int> p5 = get_next_case(p4, pydice);
+  vector<unsigned int> p6 = get_next_case(p5, pydice);
+  vector<unsigned int> p7 = get_next_case(p6, pydice);
+  vector<unsigned int> p8 = get_next_case(p7, pydice);
+  vector<unsigned int> p9 = get_next_case(p8, pydice);
+
+  vector<unsigned int> c2 = get_next_case(cudice, cudice);
+  vector<unsigned int> c3 = get_next_case(c2, cudice);
+  vector<unsigned int> c4 = get_next_case(c3, cudice);
+  vector<unsigned int> c5 = get_next_case(c4, cudice);
+  vector<unsigned int> c6 = get_next_case(c5, cudice);
+
+/*
+  for(int i=0; i < pydice_case.size(); i++)
+    cout << pydice_case[i] << ",";
+  cout << endl;
+*/
+
+cout << "p9.size=" << p9.size() << " c6.size=" << c6.size() << endl;
+
+  map<unsigned int, unsigned int> map_p9;
+  map<unsigned int, unsigned int> map_c6;
+
+  for(int i=0; i < p9.size(); i++)
+  {
+    unsigned int dice_sum = p9[i];
+    auto iter = map_p9.find(dice_sum);
+    if (iter == map_p9.end())
+    {
+      map_p9[dice_sum] = 1;
+    }
+    else
+    {
+      iter->second++;
+    }
+  }
+
+/*
+  for(auto iter = map_p9.begin(); iter != map_p9.end(); iter++)
+    cout << "(" << iter->first << "," << iter->second << ")";
+  cout << endl;
+  */
+  
+  for(int i=0; i < c6.size(); i++)
+  {
+    unsigned int dice_sum = c6[i];
+    auto iter = map_c6.find(dice_sum);
+    if (iter == map_c6.end())
+    {
+      map_c6[dice_sum] = 1;
+    }
+    else
+    {
+      iter->second++;
+    }
+  }
+
+
+  unsigned long long int win = 0;
+  for(auto piter = map_p9.begin(); piter != map_p9.end(); piter++)
+  {
+    for(auto citer = map_c6.begin(); citer != map_c6.end(); citer++)
+    {
+      if (piter->first > citer->first)
+      {
+        win += piter->second * citer->second;
+      }
+    }
+  }
+
+  cout << "wincount=" << win << endl;
+
+
+  cout << (float)win/whole << endl;
 
   clock_t end = clock();
   std::cout << "elapsed time=" << double( end-begin) / CLOCKS_PER_SEC << endl;
